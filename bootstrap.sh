c92286a6b960c9a8adab66c94b078b20c07bafc1
bang <sqibang@gmail.com>
2017-07-19 16:07:41 +0700

    chore: update functions and bootstrap shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,12 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+		--exclude ".bash_profile" \
+		--exclude ".bash_prompt" \
+		--exclude ".bashrc" \
+		--exclude ".tmux.conf" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	source ~/.zshrc;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
